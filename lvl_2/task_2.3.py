e22ac41eade9d71ad4d2e2f4893363393deb0cc4
AlinaGafarova <131188226+AlinaGafarova@users.noreply.github.com>
2023-05-24 15:54:35 +0500

    Update task_2.3.py
    Ред-ние 2.3

--- lvl_2/task_2.3.py
+++ lvl_2/task_2.3.py
@@ -3,31 +3,31 @@
 
 def switch_it_up(number):
     match number:
-        case '0':
+        case 0:
             return 'Zero'
-        case '1':
+        case 1:
             return 'One'
-        case '2':
+        case 2:
             return 'Two'
-        case '3':
+        case 3:
             return 'Three'
-        case '4':
+        case 4:
             return 'Four'
-        case '5':
+        case 5:
             return 'Five'
-        case '6':
+        case 6:
             return 'Six'
-        case '7':
+        case 7:
             return 'Seven'
-        case '8':
+        case 8:
             return 'Eight'
-        case '9':
+        case 9:
             return 'Nine'
         case _:
             return 'None'
 
 
-chislo = input('Введите цифру от 0 до 9: ')
+chislo = int(input('Введите цифру от 0 до 9: '))
 rezultat = switch_it_up(chislo)
 print(rezultat)
 
